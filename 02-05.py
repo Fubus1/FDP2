fa58f365b519ff50c2cf6bbd013e644bce5dcba3
Fubus1 <fabioore96@gmail.com>
2024-05-02 20:13:43 -0400

    laclase de hoy 02-05

---
+++ 02-05.py
@@ -0,0 +1,8 @@
+
+def area(r):
+    print ("El area del circulo es , ", (r**2) * 3,14)     
+
+
+print("Ingrese un numero")
+radio=int(input())
+area(radio)
--- 18-04.py
+++ 18-04.py
@@ -3,4 +3,32 @@
 
 #print(saludar("Fabio"))
 
-def area(x,y): 
+
+
+def area_cuadrado (x):
+    return(x*x);
+def perimetro_cuadrado(x):
+    return (4*x);
+
+x = int(input(" Ingrese el primer numero "))
+
+
+print("El area del cuadrado es: ", area_cuadrado(x));
+print("El perimetro del cuadrado es: ", perimetro_cuadrado(x));
+
+
+
+def area_rectangulo (x,y):
+    return(x*y);
+def perimetro_rectangulo(x,y):
+    return (2*x+2*y);
+
+x = int(input(" Ingrese el primer numero "))
+y = int(input(" Ingrese el segundo numero "))
+
+print("El area del cuadrado es: ", area_rectangulo(x,y));
+print("El perimetro del cuadrado es: ", perimetro_rectangulo(x,y));
+
+
+
+
