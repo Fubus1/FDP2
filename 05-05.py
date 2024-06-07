f1ea85f64c727c3a3567096932485096a7d4eafb
Fubus1 <fabioore96@gmail.com>
2024-06-06 20:08:46 -0400

    Clase 06-05 diccionario listas

--- 05-05.py
+++ 05-05.py
@@ -2,7 +2,7 @@
 
 #with open("archivotabla.txt", "w") as archivo:
     #for i in range (1,11):
-        #archivo.write(f"{num} x {i} = {num+i} \n")
+        #archivo.write(f"{num} x {i} = {num+i} \n") \n= salto de linea
 
 def suma (num1,num2):
     with open (f"suma{num1}mas{num2}.txt", "w") as archivo:
